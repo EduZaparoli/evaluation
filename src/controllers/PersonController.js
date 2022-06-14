7a1988d4374fb5c76d98089e4be63734cc54e927
Eduardo Zaparoli <eduzzx1204@gmail.com>
2022-06-14 10:00:46 -0300

    Adicionando pesquisa por atributos

--- src/controllers/PersonController.js
+++ src/controllers/PersonController.js
@@ -31,35 +31,38 @@ class PersonController{
     }
 
     static showPerson = async (req, res) =>{
-        await persons.findAll()
-        .then((persons) => {
-            res.status(200).json(persons);
-        }).catch(() => {
-            res.status(400).send({message: "Erro ao pesquisar!"})
-        })
-    }
-
-    static showPersonName = async (req, res) =>{
-        const { name } = req.params;
-
-        await persons.findAll ({'name' : name}, {}, (err, person) =>{
-            res.status(200).send(person); 
-        })
+        if(req.query.name!=null){
+            await persons.findAll(
+                {where : {"name": req.query.name}}
+            ).then((persons) => {
+                res.status(200).json(persons);
+            }).catch(() => {
+                res.status(400).send({message: "Erro ao pesquisar!"});
+            })
+        }
+        else if(req.query.job!=null){
+            await persons.findAll(
+                {where : {"job": req.query.job}}
+            ).then((persons) => {
+                res.status(200).json(persons);
+            }).catch(() => {
+                res.status(400).send({message: "Erro ao pesquisar!"});
+            })
+        }
+        else{
+            await persons.findAll()
+            .then((persons) => {
+                res.status(200).json(persons);
+            }).catch(() => {
+                res.status(400).send({message: "Erro ao pesquisar!"});
+            })
+        }
     }
 
     static showPersonId = async (req, res) =>{
         const { id } = req.params;
         const person = await persons.findByPk(id)
         res.status(200).json(person)
-    }
-
-    static showPersonJob = async (req, res) =>{
-        const { job } = req.params;
-
-        await persons.findAll ({'job' : job}, {}, (err, person) =>{
-        res.status(200).send(person); 
-
-        })
     }
 
 }
