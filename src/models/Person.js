4a44f80e3125964233cb1dcbddac63af2d90968e
Eduardo Zaparoli <eduzzx1204@gmail.com>
2022-06-17 13:22:54 -0300

    Definindo campos como not null

--- src/models/Person.js
+++ src/models/Person.js
@@ -3,14 +3,16 @@ import db from "../config/dbConnect.js";
 
 const persons = db.define('person', {
     id: {type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true},
-    name: {type: Sequelize.STRING},
-    birth_date: {type: Sequelize.DATE},
-    birth_place: {type: Sequelize.STRING},
+    name: {type: Sequelize.STRING, allowNull: false},
+    birth_date: {type: Sequelize.DATE, allowNull: false},
+    birth_place: {type: Sequelize.STRING, allowNull: false},
     job: {type: Sequelize.STRING},
     department_id: {type: Sequelize.INTEGER, references: {model: 'department', key: 'id'}}
 }, {
     timestamps: false,
     tableName: 'person'
 })
+
+persons.sync();
 
 export default persons;
